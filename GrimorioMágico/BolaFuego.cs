b3108bb4884da140c0831e56c5a90f95ccac7929
Valentinac2008 <valentinac613@alu.bue.edu.ar>
2026-06-30 21:03:58 -0300

    Add files via upload

---
+++ GrimorioMágico/BolaFuego.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+    public class BolaFuego : Hechizo
+    {
+        public string PropiedadEspecial { get; set; }
+
+        public override string Lanzar()
+        {
+            return "BolaFuego ejecutando acción específica";
+        }
+    }
+
+}
---
+++ GrimorioMágico/Program.cs
@@ -0,0 +1,20 @@
+﻿using ConsoleApp8;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+
+class Program
+{
+    static void Main()
+    {
+        List<Hechizo> lista = new List<Hechizo>();
+        lista.Add(new BolaFuego());
+        lista.Add(new EscudoHielo());
+
+        foreach (Hechizo item in lista) {
+            Console.WriteLine(item.Lanzar());
+        }
+
+
+    }
+}
